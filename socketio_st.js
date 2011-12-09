51b9a16c8d642050c1be7f3d8eacb907420d4674
gtk2k <gtk2k@yahoo.co.jp>
2011-12-09 12:44:20 +0900

    Signed-off-by: gtk2k <gtk2k@yahoo.co.jp> test

---
+++ socketio_st.js
@@ -0,0 +1,9 @@
+﻿var io = require('socket.io').listen(80);
+io.configure('production', function () {
+    io.set('log level', 1);
+});
+io.sockets.on('connection', function (socket) {
+    socket.on('message', function (data) {
+        socket.send(data);
+    });
+});
